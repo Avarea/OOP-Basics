5a474b5dfade06341a9effe0c80d6b2a34914746
Nikolay Boyadzhiev <boyadzhiev.nikolay@gmail.com>
2018-02-21 23:34:35 +0200

    adding new stuff

---
+++ WorkingWithAbstraction/RawData/Car.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class Car
+{
+    public string Model;
+    public int EngineSpeed;
+    public int EnginePower;
+    public int CargoWeight;
+    public string CargoType;
+    public KeyValuePair<double, int>[] Tires;
+
+    public Car(string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType, double tire1Pressure, int tire1Age, double tire2Pressure, int tire2Age, double tire3Pressure, int tire3age, double tire4Pressure, int tire4age)
+    {
+        this.Model = model;
+        this.EngineSpeed = engineSpeed;
+        this.EnginePower = enginePower;
+        this.CargoWeight = cargoWeight;
+        this.CargoType = cargoType;
+        this.Tires = new KeyValuePair<double, int>[] { KeyValuePair.Create(tire1Pressure, tire1Age), KeyValuePair.Create(tire2Pressure, tire2Age), KeyValuePair.Create(tire3Pressure, tire3age), KeyValuePair.Create(tire4Pressure, tire4age) };
+    }
+
+}
