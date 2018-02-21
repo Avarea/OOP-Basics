5ed701f6e63b658230ff069bafae5b47e0051e9d
Nikolay Boyadzhiev <boyadzhiev.nikolay@gmail.com>
2018-02-21 19:18:40 +0200

    Problem 3. Student System

---
+++ WorkingWithAbstraction/StudentSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        StudentSystem studentSystem = new StudentSystem();
+        string[] args = Console.ReadLine()?.Split();
+
+        while (args[0] != "Exit")
+        {
+            studentSystem.ParseCommand(args);
+            args = Console.ReadLine()?.Split();
+        }
+    }
+}
+
