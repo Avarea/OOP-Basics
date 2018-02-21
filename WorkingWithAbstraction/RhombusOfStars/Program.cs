278de1d445ea1531c0a016c272064b7cf78b7670
Nikolay Boyadzhiev <boyadzhiev.nikolay@gmail.com>
2018-02-21 18:39:23 +0200

    new code
    Problem 1. Rhombus of Stars

---
+++ WorkingWithAbstraction/RhombusOfStars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var stars = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= stars; i++)
+        {
+            PrintRow(stars, i);
+        }
+        for (int i = stars - 1; i > 0; i--)
+        {
+            PrintRow(stars, i);
+        }
+    }
+
+    private static void PrintRow(int rhombusSize, int rowSize)
+    {
+        for (int i = 0; i < rhombusSize - rowSize; i++)
+        {
+            Console.Write(" ");
+        }
+
+        for (int i = 0; i < rowSize; i++)
+        {
+            Console.Write("* ");
+        }
+        Console.WriteLine();
+    }
+}
+
