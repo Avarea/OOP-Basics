a6a18ff291d84fe3a2109bd015bbff4c80653f4b
Nikolay Boyadzhiev <boyadzhiev.nikolay@gmail.com>
2018-02-21 18:58:29 +0200

    Problem 2. Point in Rectangle

---
+++ WorkingWithAbstraction/PointInRectangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var coodrs = Console.ReadLine().Split().Select(int.Parse).ToList();
+        Rectangle rectangle = new Rectangle(coodrs[0], coodrs[1], coodrs[2], coodrs[3]);
+
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            var cmdPoint = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int pointX = cmdPoint[0];
+            int pointY = cmdPoint[1];
+            Point point = new Point(pointX, pointY);
+
+            Console.WriteLine(rectangle.Contains(point));
+        }
+    }
+}
+
